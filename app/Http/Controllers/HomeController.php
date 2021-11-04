5190422955ad6873d05da99e462aeaa0d25d85e2
Garbahaggie <bbaranyi55@gmail.com>
2021-11-04 16:41:08 +0100

    Home page now shows subscribed subreddit posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Subreddit;
 
 class HomeController extends Controller
 {
     public function index() {
-        $posts = Post::orderBy('created_at','desc')->paginate(10);
+        $posts = new \Illuminate\Database\Eloquent\Collection;;
+        if (Auth::user() != null) {
+            $ids = collect([]);
+            $subreddits = Auth::user()->subscriptions()->get();         
+            foreach ($subreddits as $subreddit) {
+                $ids[] = ($subreddit->id);               
+            }
+            $posts = Post::whereIn('subreddit_id',$ids)->orderBy('created_at','desc')->paginate(10);
+        }
+        else {
+            $posts = Post::orderBy('created_at','desc')->paginate(10);
+        }
+        
         return view('home')->with(compact('posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="row">
-    <div class="col-md-6 mx-auto">
-        @foreach($posts as $post)
-            @include('posts._item')
-        @endforeach
-    </div>
-</div>
-<div class="d-flex justify-content-center">
-    {{ $posts->links() }}
-</div>
+<h1 class="text-center mb-5">{{ __('Recent posts from your subscribed subreddits') }}</h1>
+@if ($posts->count())
+        <div class="row">
+            <div class="mx-auto">
+                @foreach ($posts as $post)
+                    @include('posts._item')
+                @endforeach
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            {{ $posts->links() }}
+        </div>
+    @else
+        <div class="alert alert-warning">
+            {{ __('No posts to show') }}
+        </div>
+    @endif
 @endsection
